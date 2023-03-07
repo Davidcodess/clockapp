9b64b2d79e237458ebea1390c01fe90b1c960a3d
David Johnson <dmjohnson844@gmail.com>
2023-03-07 16:00:45 -0500

    continued styling development

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-
+import downArrow from './DownArrowIcon.svg';
 
 
 function App() {
@@ -14,12 +14,12 @@ function App() {
         <div className="time-container">
           <h1>GOOD MORNING </h1>
             <br></br>
-            <span>12:52AM</span>
-            <h2>In Indianapolis</h2>
+            <span>12:52</span>
+            <h2>IN INDIANAPOLIS</h2>
         </div>
 
-        <button className="drop-down-button"></button>
-
+        <button className="drop-down-button"> <p className="btn-text">More</p> <img className="arrow-btn" src={downArrow} alt="down icon"></img></button>
+    
       </div>
       
 
