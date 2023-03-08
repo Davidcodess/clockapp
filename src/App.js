4ba0999f2a46c4a560302ecb4338ce95f1e3d1fe
David Johnson <dmjohnson844@gmail.com>
2023-03-08 00:19:56 -0500

    continued development

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import downArrow from './DownArrowIcon.svg';
+import Sun from './icon-sun.svg';
 
 
 function App() {
@@ -12,15 +13,26 @@ function App() {
         </div>
 
         <div className="time-container">
-          <h1>GOOD MORNING </h1>
+        
+       
+        <h1>GOOD MORNING </h1>
             <br></br>
             <span>12:52</span>
             <h2>IN INDIANAPOLIS</h2>
         </div>
 
-        <button className="drop-down-button"> <p className="btn-text">More</p> <img className="arrow-btn" src={downArrow} alt="down icon"></img></button>
+        <button className="drop-down-button"> 
+          <div> 
+            <p className="btn-text">More</p>
+          </div> 
+
+          <div>
+            <img className="arrow-btn" src={downArrow} alt="down icon"></img>
+          </div>
+        </button>
     
       </div>
+      
       
 
 
